a6b9ea5e8375f6f9a4524a558d407266b22f2c94
Anantz <anankhaita55@gmail.com>
2019-11-16 12:40:50 +0700

    connectDb

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var db = mysql.createPool({
+  host: 'localhost',
+  user: 's59160143',
+  password: '060423561',
+  database: 's59160143',
+  debug: false
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/testconnect', function (req, res, next) {
+  if(db != null) {
+    res.send('connect success');
+  }else{
+    res.send('connect fail');
+  }
 });
 
 module.exports = router;
